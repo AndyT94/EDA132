3dce695e8f9243f7c1680d708e46daba0dab2513
Andy Truong <dat13at1@hacke-12.student.lth.se>
2017-02-10 09:21:58 +0100

    created classes

---
+++ DecisionTree/src/decisiontree/Relation.java
@@ -0,0 +1,16 @@
+package decisiontree;
+
+import java.util.List;
+
+public class Relation {
+	private String name;
+	private List<Attribute> attributes;
+	private List<Data> data;
+	
+	public Relation(String name, List<Attribute> attributes, List<Data> data) {
+		this.name = name;
+		this.attributes = attributes;
+		this.data = data;
+	}
+	
+}
